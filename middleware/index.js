0aad4788c35d2f6188c201dfb47b81b324f13141
muhammadtrionovrian-tech <muhammadtrionovrian28@gmail.com>
2025-12-11 21:07:52 +0700

    Ready to deploy

--- middleware/index.js
+++ middleware/index.js
@@ -75,7 +75,7 @@ const validateRegisterInput = (req, res, next) => {
     next();
 };
 
-// Middleware opsional untuk validasi login input
+// Middleware opsional untukk validasi login input
 const validateLoginInput = (req, res, next) => {
     const { identifier, password } = req.body;
 
